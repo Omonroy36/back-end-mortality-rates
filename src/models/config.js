94500df419dfd3061b182d048036c5765ffdebd8
Orlando Monrroy <monroyorlando.36@gmail.com>
2022-09-05 09:56:49 -0400

    adding a .env example

--- src/models/config.js
+++ src/models/config.js
@@ -1,8 +1,11 @@
 import Sequelize from "sequelize";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize("test", "postgres", "admin", {
-  host: "localhost",
-  dialect: "postgres",
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
   pool: {
     max: 5,
     min: 0,
